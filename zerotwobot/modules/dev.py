d3dfa285dae0c69fd45b54e5c8cafb73973f79ae
Joker Hacker <jokerhacker.6521@protonmail.com>
2022-04-11 00:57:26 +0530

    devs: remove gitpull and restart commands since we creating new bot for that

--- zerotwobot/modules/dev.py
+++ zerotwobot/modules/dev.py
@@ -51,45 +51,13 @@ def leave(update: Update, context: CallbackContext):
 
 
 
-@dev_plus
-def gitpull(update: Update, context: CallbackContext):
-    sent_msg = update.effective_message.reply_text(
-        "Pulling all changes from remote and then attempting to restart.",
-    )
-    subprocess.Popen("git pull", stdout=subprocess.PIPE, shell=True)
-
-    sent_msg_text = sent_msg.text + "\n\nChanges pulled...I guess.. Restarting in "
-
-    for i in reversed(range(5)):
-        sent_msg.edit_text(sent_msg_text + str(i + 1))
-        sleep(1)
-
-    sent_msg.edit_text("Restarted.")
-
-    os.system("restart.bat")
-    os.execv("start.bat", sys.argv)
-
-
-
-@dev_plus
-def restart(update: Update, context: CallbackContext):
-    update.effective_message.reply_text(
-        "Starting a new instance and shutting down this one",
-    )
-
-    os.system("restart.bat")
-    os.execv("start.bat", sys.argv)
-
-
 LEAVE_HANDLER = CommandHandler("leave", leave, run_async=True)
-GITPULL_HANDLER = CommandHandler("gitpull", gitpull, run_async=True)
-RESTART_HANDLER = CommandHandler("reboot", restart, run_async=True)
+
 ALLOWGROUPS_HANDLER = CommandHandler("lockdown", allow_groups, run_async=True)
 
 dispatcher.add_handler(ALLOWGROUPS_HANDLER)
 dispatcher.add_handler(LEAVE_HANDLER)
-dispatcher.add_handler(GITPULL_HANDLER)
-dispatcher.add_handler(RESTART_HANDLER)
+
 
 __mod_name__ = "Dev"
-__handlers__ = [LEAVE_HANDLER, GITPULL_HANDLER, RESTART_HANDLER, ALLOWGROUPS_HANDLER]
+__handlers__ = [LEAVE_HANDLER, ALLOWGROUPS_HANDLER]
