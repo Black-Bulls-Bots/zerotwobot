a441c5eb490b54cbb376cf7d7921e1dc177f0124
Joker Hacker <jokerhacker.6521@protonmail.com>
2023-01-22 00:31:28 +0530

    translate: sick of moving from one library to another
    Just forked the original google_trans_new inside bot repo

---
+++ zerotwobot/modules/helper_funcs/translator/__init__.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+# author LuShan
+# version : 1.1.9
+
+"I'm just sick of moving from version to version of different projects"
+
+from .google_trans_new import google_translator
+from .constant import DEFAULT_SERVICE_URLS, LANGUAGES
